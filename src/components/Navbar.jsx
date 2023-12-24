e297c7525703da64b357a1ee2025dca91e6ff566
aakash-sharma25 <as024harma@gmail.com>
2023-12-24 20:29:37 +0530

    added navbar and footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,66 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import "../App.css"
-import { GiHamburgerMenu } from 'react-icons/gi';
-import Drawer from './Drawer';
+import React, { useEffect, useState } from "react";
+import "../App.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import Drawer from "./Drawer";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Navbar = () => {
-    const name ="<> Aakash </>";
+  const name = "<> Aakash </>";
 
-    const [open,setopen] =useState(false);
+  const [open, setopen] = useState(false);
 
-    const toggle = () =>{
+  const toggle = () => {
+    setopen((prev) => !prev);
+  };
 
-        setopen( (prev)=> !prev)
-    
-      }
-
-      useEffect(()=>{
-        AOS.init();
-        AOS.refresh();
-      },[])
-
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
 
   return (
-    <div  
-    // data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500"
-     className=' font-bold navbar flex justify-between text-xl items-center p-[30px] w-[100%] fixed top-0 left-0 right-0 z-10  bg-blue-500 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]'>
-        
-        <h1 className='text-white text-3xl logo'>{name}</h1>
-        <div className='nav '>
-            <ul className='flex gap-5 '>
-                <li  className='hover:text-white transition ease-in-out delay-100'>
-                    <a  href='# ' >Home</a>
-                </li>
-                <li className='hover:text-white transition ease-in-out delay-100'>
-                    <a  href='#about'  >About</a>
-                </li>
-                <li className='hover:text-white transition ease-in-out delay-100'>
-                    <a  href='#project' >Project</a>
-                </li>
-                <li className='hover:text-white transition ease-in-out delay-100'>
-                    <a  href='#contact' >Contact Me</a>
-                </li>
-            </ul>
-        </div>
+    <div
+      // data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500"
+      className=" font-bold navbar flex justify-between text-xl items-center p-[30px] w-[100%] fixed top-0 left-0 right-0 z-10  bg-blue-500 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]"
+    >
+      <h1 className="text-white text-3xl logo">{name}</h1>
+      <div className="nav ">
+        <ul className="flex gap-5 ">
+          <li className="hover:text-white transition ease-in-out delay-100">
+            <a href="# ">Home</a>
+          </li>
+          <li className="hover:text-white transition ease-in-out delay-100">
+            <a href="#about">About</a>
+          </li>
+          <li className="hover:text-white transition ease-in-out delay-100">
+            <a href="#project">Project</a>
+          </li>
+          <li className="hover:text-white transition ease-in-out delay-100">
+            <a href="#contact">Contact Me</a>
+          </li>
+        </ul>
+      </div>
 
-        <div className='hamburger'>
-            <GiHamburgerMenu onClick={toggle}/>
+      <div className="hamburger">
+        <GiHamburgerMenu onClick={toggle} />
+      </div>
 
-        </div>
-
-       
-            <div className=' bg-white absolute top-[100%] left-[0px]  w-[100vw] text-center'>
-
-           
-            {
-                open &&  <Drawer setopen={setopen}/>
-            }
-            </div>
-        
+      <div className=" bg-white absolute top-[100%] left-[0px]  w-[100vw] text-center">
+        {open && <Drawer setopen={setopen} />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
