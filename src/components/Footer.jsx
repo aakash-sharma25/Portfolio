703fb25b00c375760f40484ec0c3e8f9146d96e1
aakash-sharma25 <as024harma@gmail.com>
2023-12-24 20:28:36 +0530

    added navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,24 @@
-import Social from "./Social"
+import Social from "./Social";
 
 const Footer = () => {
-    const date =new Date();
-    return (
-        <div data-aos="fade-up" data-aos-easing="linear" data-aos-duration="500"
-         className=" mt-20 py-10 flex flex-col gap-20 items-center justify-center bg-slate-100">
+  const date = new Date();
+  return (
+    <div
+      data-aos="fade-up"
+      data-aos-easing="linear"
+      data-aos-duration="500"
+      className=" mt-20 py-10 flex flex-col gap-20 items-center justify-center bg-slate-100"
+    >
+      <div className="mt-5 flex ">
+        <Social />
+      </div>
+      <div className="text-center">
+        <p className="text-xl text-gray-500">
+          © {date.getFullYear()} Aakash Sharma . All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-            <div className="mt-5 flex ">
-
-                <Social />
-                
-            </div>
-                <div className="text-center">
-                    <p className="text-xl text-gray-500">© {date.getFullYear()} Aakash Sharma . All rights reserved.</p>
-                </div>
-
-        </div>
-    )
-}
-
-export default Footer
+export default Footer;
