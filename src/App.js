ba131a9f002b6fad2e0588b5288613ff328e8eb4
aakash-sharma25 <as024harma@gmail.com>
2024-01-10 18:28:41 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import './App.css';
-import About from './components/About';
-import Contact from './components/Contact';
-import Content from './components/Content';
-import Experience from './components/Experience';
-import Footer from './components/Footer';
-import Hirebtn from './components/Hirebtn';
-import Navbar from './components/Navbar';
-import Skills from './components/Skills';
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Content from "./components/Content";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import Hirebtn from "./components/Hirebtn";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div>
-    <div className='fixed right-[60px] bottom-[60px] hire z-50'>
+      <div className="fixed right-[60px] bottom-[60px] hire z-50">
+        <Hirebtn name="Hire me" link="https://www.linkedin.com/in/aakash25/" />
+      </div>
 
-      <Hirebtn  name="Hire me" link="https://www.linkedin.com/in/aakash25/" />
-      
+      <Navbar />
+      <Content />
+      <About />
+      <Experience />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
-      
-      <Navbar/>
-
-      <Content/>
-      <About/>
-      <Experience/>
-      <Skills/>
-      <Contact/>
-      <Footer/>
-    </div>
-    
   );
 }
 
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import "../App.css"
-const Drawer = ({setopen}) => {
+import React from "react";
+import "../App.css";
+const Drawer = ({ setopen }) => {
   return (
-    <div className='py-5 transition-all '>
-         <ul className='flex flex-col gap-10 '>
-                <li  className='hover:text-blue-500'>
-                    <a  href='# 'onClick={()=> setopen(false)}>Home</a>
-                </li>
-                <li className='hover:text-blue-500'>
-                    <a  href='#about' onClick={()=> setopen(false)}>About</a>
-                </li>
-                <li className='hover:text-blue-500'>
-                    <a  href='#project'onClick={()=> setopen(false)}>Project</a>
-                </li>
-                <li className='hover:text-blue-500'>
-                    <a  href='#contact'onClick={()=> setopen(false)}>Contact Me</a>
-                </li>
-            </ul>
+    <div className="py-5 transition-all ">
+      <ul className="flex flex-col gap-10 ">
+        <li className="hover:text-blue-500">
+          <a href="# " onClick={() => setopen(false)}>
+            Home
+          </a>
+        </li>
+        <li className="hover:text-blue-500">
+          <a href="#about" onClick={() => setopen(false)}>
+            About
+          </a>
+        </li>
+        <li className="hover:text-blue-500">
+          <a href="#project" onClick={() => setopen(false)}>
+            Project
+          </a>
+        </li>
+        <li className="hover:text-blue-500">
+          <a href="#contact" onClick={() => setopen(false)}>
+            Contact Me
+          </a>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Drawer
+export default Drawer;
